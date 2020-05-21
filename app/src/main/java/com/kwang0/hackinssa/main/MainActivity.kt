8922bebb798a26d61d95caacf8c8481138df3cd0
KWANGYOUNGCHOI-butake <kwangyoung.choi@butake.com>
2020-05-21 23:05:39 +0900

    features/cky/20.05.21 Update MainAct TabLayout Style

--- app/src/main/java/com/kwang0/hackinssa/main/MainActivity.kt
+++ app/src/main/java/com/kwang0/hackinssa/main/MainActivity.kt
@@ -8,12 +8,10 @@ import com.kwang0.hackinssa.R
 
 class MainActivity: AppCompatActivity() {
 
-    lateinit var bnv: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        bnv = findViewById(R.id.main_bnv);
     }
 }
