d3a1467940ee36773cb8a5e877176c2e6b5f3abe
KWANGYOUNGCHOI-butake <kwangyoung.choi@butake.com>
2020-05-23 01:36:18 +0900

    features/cky/20.05.23 Error fix network connection with rxjava single

--- app/src/main/java/com/kwang0/hackinssa/presentation/ui/activities/countryselect/CountrySelectActivity.kt
+++ app/src/main/java/com/kwang0/hackinssa/presentation/ui/activities/countryselect/CountrySelectActivity.kt
@@ -1,21 +1,30 @@
 package com.kwang0.hackinssa.presentation.ui.activities.countryselect
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import com.kwang0.hackinssa.R
 import com.kwang0.hackinssa.data.models.Country
 import com.kwang0.hackinssa.presentation.ui.activities.BaseActivity
 import com.kwang0.hackinssa.presentation.ui.adapters.CountryAdapter.Companion.BASE_IMG_URL_250_PX
 import com.squareup.picasso.Picasso
+import io.reactivex.Observable
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import org.apache.commons.net.ntp.NTPUDPClient
+import java.io.IOException
 import java.net.InetAddress
 import java.util.*
 
 
 class CountrySelectActivity : BaseActivity() {
     val TAG = CountrySelectActivity::class.simpleName
+
+    lateinit var time_tv: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,7 +34,7 @@ class CountrySelectActivity : BaseActivity() {
 
         val iv = findViewById<ImageView>(R.id.cs_iv)
         val name_tv = findViewById<TextView>(R.id.cs_name_tv)
-        val time_tv = findViewById<TextView>(R.id.cs_time_tv)
+        time_tv = findViewById<TextView>(R.id.cs_time_tv)
 
         Picasso.get()
                 .load(BASE_IMG_URL_250_PX.toString() + country.getAlpha2Code()!!.toLowerCase() + ".png?raw=true")
@@ -33,14 +42,23 @@ class CountrySelectActivity : BaseActivity() {
 
         name_tv.text = country.getNativeName()
 
+        getNetworkTime()
+    }
 
-//        val timeClient = NTPUDPClient()
-//        val inetAddress: InetAddress = InetAddress.getByName(TIME_SERVER)
-//        val timeInfo = timeClient.getTime(inetAddress)
-//        val returnTime = timeInfo.returnTime
-//        Log.d(TAG, "time : " + returnTime)
-//        val time = Date(returnTime)
-//        time_tv.text = time.time.toString()
+    @SuppressLint("CheckResult")
+    @Throws(IOException::class)
+    fun getNetworkTime() {
+        Single.just(true)
+                .observeOn(Schedulers.computation())
+                .subscribeOn(AndroidSchedulers.mainThread())
+                .subscribe({ result ->
+                    val timeClient = NTPUDPClient()
+                    val inetAddress = InetAddress.getByName(TIME_SERVER)
+                    val timeInfo = timeClient.getTime(inetAddress)
+
+                    val time = Date(timeInfo.message.receiveTimeStamp.time)
+                    time_tv.text = time.toString()
+                }) { error -> Toast.makeText(this, "get Error!", Toast.LENGTH_SHORT).show() }
     }
 
     companion object {
