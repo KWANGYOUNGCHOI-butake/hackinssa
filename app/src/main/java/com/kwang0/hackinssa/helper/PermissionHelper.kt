86db1db73918578941e91b242fb65c521d6c7c09
KWANGYOUNGCHOI <cky1203@gmail.com>
2020-05-31 17:00:05 +0900

    features/cky/20.05.31 PermissionHelper 에 주석 추가

--- app/src/main/java/com/kwang0/hackinssa/helper/PermissionHelper.kt
+++ app/src/main/java/com/kwang0/hackinssa/helper/PermissionHelper.kt
@@ -10,8 +10,8 @@ import androidx.core.content.ContextCompat
 object PermissionHelper {
     const val READ_STORAGE_REQUEST_CODE = 4000
 
+    // Read External Storage Permission 이 체크 되어있는지 확인
     fun readStoragePermissionCheck(activity: Activity): Boolean {
-        // Here, thisActivity is the current activity
         if (ContextCompat.checkSelfPermission(activity,
                         Manifest.permission.READ_EXTERNAL_STORAGE)
                 != PackageManager.PERMISSION_GRANTED) {
@@ -22,6 +22,7 @@ object PermissionHelper {
         return true
     }
 
+    // Read EXternal Storage 에 대한 리퀘스트 요청
     fun showReadStorageRequest(activity: Activity) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
             ActivityCompat.requestPermissions(activity,
