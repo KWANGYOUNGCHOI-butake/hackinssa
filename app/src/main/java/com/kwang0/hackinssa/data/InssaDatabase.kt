a188a685a3675bf14cb5bb4871b49fab153f0e18
KWANGYOUNGCHOI <cky1203@gmail.com>
2020-06-11 22:58:48 +0900

    features/cky/20.06.11 Update SingletonHolder.kt 향상

--- app/src/main/java/com/kwang0/hackinssa/data/InssaDatabase.kt
+++ app/src/main/java/com/kwang0/hackinssa/data/InssaDatabase.kt
@@ -10,6 +10,8 @@ import com.kwang0.hackinssa.data.dao.TagDao
 import com.kwang0.hackinssa.data.models.Favorite
 import com.kwang0.hackinssa.data.models.Friend
 import com.kwang0.hackinssa.data.models.Tag
+import com.kwang0.hackinssa.helper.SingletonArgHolder
+import com.kwang0.hackinssa.helper.SingletonHolder
 
 // database singleton
 @Database(entities = [Friend::class, Tag::class, Favorite::class], version = 1)
@@ -18,22 +20,7 @@ abstract class InssaDatabase : RoomDatabase() {
     abstract fun favoriteDao(): FavoriteDao
     abstract fun tagDao(): TagDao
 
-    companion object {
-        // 멀티 쓰레드 환경에서 발생하는 문제를 해결하기 위해
-        // volatile 을 사용해서 변수의 read와 write를 Main Memory에서 진행
-        @Volatile
-        private var INSTANCE: InssaDatabase? = null
-
-        @Synchronized
-        fun getInstance(context: Context): InssaDatabase =
-                INSTANCE ?: synchronized(this) {
-                    INSTANCE ?: buildDatabase(context).also { INSTANCE = it }
-                }
-
-        private fun buildDatabase(context: Context) =
-                Room.databaseBuilder(context.applicationContext,
-                        InssaDatabase::class.java, "Sample.db")
-                        .build()
-
-    }
+    companion object : SingletonArgHolder<InssaDatabase, Context>({
+        Room.databaseBuilder(it.applicationContext, InssaDatabase::class.java, "Sample.db").build()
+    })
 }
--- app/src/main/java/com/kwang0/hackinssa/data/RetrofitApi.kt
+++ app/src/main/java/com/kwang0/hackinssa/data/RetrofitApi.kt
@@ -1,32 +1,22 @@
 package com.kwang0.hackinssa.data
 
 import android.content.Context
+import androidx.room.Room
 import com.kwang0.hackinssa.data.remote.CountryRepositoryRemote
+import com.kwang0.hackinssa.helper.SingletonHolder
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 // retrofit singleton
 abstract class RetrofitApi {
+    companion object : SingletonHolder<Retrofit>({
+        val BASE_URL = "https://restcountries.eu/rest/v2/"
 
-    companion object {
-        const val BASE_URL = "https://restcountries.eu/rest/v2/"
-        // 멀티 쓰레드 환경에서 발생하는 문제를 해결하기 위해
-        // volatile 을 사용해서 변수의 read와 write를 Main Memory에서 진행
-        @Volatile
-        private var retrofit : Retrofit? = null
-
-        @Synchronized
-        fun getInstance(): Retrofit =
-                retrofit ?: synchronized(this) {
-                    retrofit ?: buildRetrofit().also { retrofit = it }
-                }
-
-        private fun buildRetrofit() = Retrofit.Builder()
-                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .baseUrl(BASE_URL)
-                    .build()
-
-    }
+        Retrofit.Builder()
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl(BASE_URL)
+                .build()
+    })
 }
