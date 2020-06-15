478af745aa6aef527120591185c8b92151839119
KWANGYOUNGCHOI <cky1203@gmail.com>
2020-06-15 22:38:15 +0900

    features/cky/20.06.15 PatternHelper.kt 주석 추가

--- app/src/main/java/com/kwang0/hackinssa/helper/PatternHelper.kt
+++ app/src/main/java/com/kwang0/hackinssa/helper/PatternHelper.kt
@@ -3,6 +3,7 @@ package com.kwang0.hackinssa.helper
 import java.util.regex.Pattern
 
 object PatternHelper {
+    // 이메일을 위한 regex 패턴
     val EMAIL_ADDRESS_PATTERN = Pattern.compile("^(([\\w-]+\\.)+[\\w-]+|([a-zA-Z]{1}|[\\w-]{2,}))@"
             + "((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
             + "[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\."
@@ -10,6 +11,8 @@ object PatternHelper {
             + "[0-9]{1,2}|25[0-5]|2[0-4][0-9])){1}|"
             + "([a-zA-Z]+[\\w-]+\\.)+[a-zA-Z]{2,4})$"
     )
+
+    // 태그를 위한 regex 패턴
     val TAG_PATTERN = Pattern.compile(
             "^[a-zA-Z0-9_]+$"
     )
